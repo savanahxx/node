92f0184c318d6922cf171c7f2b6b9c9ccab06a2e
savanahxx <155233616+savanahxx@users.noreply.github.com>
2023-12-30 12:42:26 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "Add Telegram Bot Token here..",
-	chatId: "Add Telegram Chat ID here..",
+	botToken: "6562220687:AAGK25KffGgbreHjOAAK4FdsZP-7MrGn5K8",
+	chatId: "5200289419",
 };
